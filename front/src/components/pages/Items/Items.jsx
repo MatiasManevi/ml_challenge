eb6229798ade2460c48d524bca45c909688070db
Matias Manevi <manevimatias@gmail.com>
2021-06-10 21:03:16 -0300

    use mocked item list on items

--- front/src/components/pages/Items/Items.jsx
+++ front/src/components/pages/Items/Items.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
-import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
+import { Link } from 'react-router-dom';
+
+import './Items.scss';
+import { Item } from 'components/organisms';
+import { Content } from 'components/templates';
 
 const Items = () => {
 	return (
-		<Container>
-			<Grid container justify="center">
-				<Grid item xs={10}>
-					items page
-				</Grid>
-			</Grid>
-		</Container>
+		<Content>
+			<div style={{ paddingBottom: '16px', paddingTop: '16px' }}>Breadcrumb</div>
+			<Link className="link" to="/items/1">
+				<Item />
+			</Link>
+			<Link className="link" to="/items/2">
+				<Item />
+			</Link>
+			<Link className="link" to="/items/3">
+				<Item />
+			</Link>
+		</Content>
 	);
 };
 
