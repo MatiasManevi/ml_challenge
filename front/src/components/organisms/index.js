254a621b855a3d71e1b990271448286eb2445700
Matias Manevi <manevimatias@gmail.com>
2021-06-12 12:20:49 -0300

    Export bcrumb component as organism

--- front/src/components/organisms/index.js
+++ front/src/components/organisms/index.js
@@ -1,2 +1,3 @@
 export { default as Header } from 'components/organisms/Header';
 export { default as Item } from 'components/organisms/Item';
+export { default as BreadcrumbMenu } from 'components/organisms/BreadcrumbMenu';
