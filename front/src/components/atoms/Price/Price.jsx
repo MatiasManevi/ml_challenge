5324ef98064fb15fb0baf3cd93e4c51d0fa97082
Matias Manevi <manevimatias@gmail.com>
2021-06-12 02:25:32 -0300

    Update decimal tag in Price component

--- front/src/components/atoms/Price/Price.jsx
+++ front/src/components/atoms/Price/Price.jsx
@@ -4,10 +4,13 @@ import './Price.scss';
 
 const Price = ({ price, small = true, freeShipping = false }) => {
 	return (
-		<>
-			<p className={small ? 'price--small' : 'price--big'}>$ {price}</p>
+		<div className="price">
+			<p className={small ? 'price--small' : 'price--big'}>
+				$ 1.222
+				<sup className={small ? 'price__cents--small' : 'price__cents--big'}>23</sup>
+			</p>
 			{freeShipping && <img className="price__shipping" src="ic_shipping.png" />}
-		</>
+		</div>
 	);
 };
 
--- front/src/components/atoms/index.js
+++ front/src/components/atoms/index.js
@@ -2,3 +2,4 @@ export { default as Logo } from 'components/atoms/Logo';
 export { default as Price } from 'components/atoms/Price';
 export { default as SearchInput } from 'components/atoms/SearchInput';
 export { default as SearchButton } from 'components/atoms/SearchButton';
+export { default as PrimaryButton } from 'components/atoms/PrimaryButton';
