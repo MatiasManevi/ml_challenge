2c5c94a98f82a588d2e2357d04dfbcb38dd4e012
Matias Manevi <manevimatias@gmail.com>
2021-06-13 22:08:12 -0300

    Improve styling

--- front/src/components/atoms/Price/Price.jsx
+++ front/src/components/atoms/Price/Price.jsx
@@ -7,7 +7,13 @@ const Price = ({ price, small = true, freeShipping = false }) => {
 		<div className="price">
 			<p className={small ? 'price--small' : 'price--big'}>
 				$ 1.222
-				<sup className={small ? 'price__cents--small' : 'price__cents--big'}>23</sup>
+				<sup
+					className={
+						'price__cents ' + (small ? 'price__cents--small' : 'price__cents--big')
+					}
+				>
+					23
+				</sup>
 			</p>
 			{freeShipping && <img className="price__shipping" src="ic_shipping.png" />}
 		</div>
