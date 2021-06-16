fa4a915e5af59f6c3e4acf4f8120e5510f340f78
Matias Manevi <manevimatias@gmail.com>
2021-06-15 23:58:23 -0300

    Comment item controller method and pass down the req params

--- back/controllers/items/index.js
+++ back/controllers/items/index.js
@@ -1,26 +1,45 @@
+/**
+ * Contollers - take request object, pull out data from request, then send to service(s).
+ * Express logic "ends" right here in the controllers. Services and lower layers knows nothing about upper layers.
+ */
+
 const { itemsService } = require('../../services');
 
 const { getItems, getItemDetail } = itemsService;
 
+/**
+ * Given a search query, it Returns an array of items that matches it
+ * @param {*} req
+ * @param {*} res
+ * @param {*} next
+ */
 const get = async (req, res, next) => {
 	try {
-		const items = await getItems();
+		const { search } = req.query;
+		const items = await getItems(search);
 		res.send(items);
 		next();
 	} catch (e) {
-		console.log(e.message);
-		res.sendStatus(500) && next(error);
+		console.error(e);
+		res.sendStatus(500);
 	}
 };
 
+/**
+ * Given an id, it returns a detailed item record
+ * @param {*} req
+ * @param {*} res
+ * @param {*} next
+ */
 const getById = async (req, res, next) => {
 	try {
-		const item = await getItemDetail();
+		const { id } = req.params;
+		const item = await getItemDetail(id);
 		res.send(item);
 		next();
 	} catch (e) {
-		console.log(e.message);
-		res.sendStatus(500) && next(error);
+		console.error(e.message);
+		res.sendStatus(500);
 	}
 };
 
