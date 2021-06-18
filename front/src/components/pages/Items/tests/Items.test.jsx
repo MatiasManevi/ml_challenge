bd212477abd210b968a69cc9bc8b7ebb6ce9a497
Matias Manevi <manevimatias@gmail.com>
2021-06-17 23:43:17 -0300

    Update items unit test

--- front/src/components/pages/Items/tests/Items.test.jsx
+++ front/src/components/pages/Items/tests/Items.test.jsx
@@ -2,6 +2,16 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import Items from '../Items';
+import * as hooks from '../hooks';
+import items from './items.fixtures';
+
+jest.mock('../hooks');
+
+const stateSpy = jest.spyOn(hooks, 'useItems');
+
+stateSpy.mockReturnValue({
+	items
+});
 
 describe('Items', () => {
 	it('should match snapshot', () => {
---
+++ front/src/components/pages/Items/tests/items.fixtures.js
@@ -0,0 +1,34 @@
+export default [
+	{
+		id: 'MLA923008999',
+		title: 'Cacerola 24 Cm Essen Aqua Con Antiadherente ',
+		price: { currency: '$', amount: 26399, decimals: '00' },
+		picture: 'http://http2.mlstatic.com/D_712413-MLA46206646617_052021-O.jpg',
+		condition: 'new',
+		free_shipping: true
+	},
+	{
+		id: 'MLA920404456',
+		title: 'Essen Set Cacerola Sarten 24 Y Cacerola 18 Mas Utensilios',
+		price: { currency: '$', amount: 75886, decimals: '00' },
+		picture: 'http://http2.mlstatic.com/D_985487-MLA45983108078_052021-O.jpg',
+		condition: 'new',
+		free_shipping: true
+	},
+	{
+		id: 'MLA839886481',
+		title: 'Set Aqua Essen 3 Piezas + Set De Utensilios Super Oferta',
+		price: { currency: '$', amount: 75950, decimals: '00' },
+		picture: 'http://http2.mlstatic.com/D_750416-MLA44276051760_122020-O.jpg',
+		condition: 'new',
+		free_shipping: true
+	},
+	{
+		id: 'MLA910856297',
+		title: 'Essen Set Marsala 2 Piezas 28 Cm C/ant Nova Trimium Oferta!!',
+		price: { currency: '$', amount: 62750, decimals: '00' },
+		picture: 'http://http2.mlstatic.com/D_861354-MLA45116844792_032021-O.jpg',
+		condition: 'new',
+		free_shipping: true
+	}
+];
