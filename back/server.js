0f53066ddb60ce4ac2e43c9edccfbd807698db4c
Matias Manevi <manevimatias@gmail.com>
2021-06-17 23:15:32 -0300

    Add cors

--- back/server.js
+++ back/server.js
@@ -1,9 +1,11 @@
+const cors = require('cors');
+
 const app = require('./setup-express');
 const routes = require('./routes');
 const { PORT } = require('./config');
 const setAuthor = require('./middlewares/setAuthor');
 
-app.get('/', (_, res) => res.send('App is working'));
+app.use(cors());
 app.use('/api', setAuthor);
 app.use('/api', routes);
 app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
