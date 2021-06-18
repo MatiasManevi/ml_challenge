db69902e1b1ac3bc125ddee0a985efe0905fab55
Matias Manevi <manevimatias@gmail.com>
2021-06-18 00:10:33 -0300

    Add alt attr

--- front/src/components/organisms/Item/Item.jsx
+++ front/src/components/organisms/Item/Item.jsx
@@ -8,7 +8,11 @@ import { Price } from 'components/atoms';
 const Item = ({ item }) => {
 	return (
 		<Paper className="item">
-			<img src={item.picture} alt="" className="item__thumbnail" />
+			<img
+				src={item.picture}
+				alt={`imagen de ${item.title}`}
+				className="item__thumbnail"
+			/>
 			<div className="item__content">
 				<div className="item__description">
 					<Grid container alignItems="baseline">
