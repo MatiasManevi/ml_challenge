624322f4bb7fbf083ca34e1078f1760823a850e1
Matias Manevi <manevimatias@gmail.com>
2021-06-19 02:20:50 -0300

    Serve statics not on development env

--- back/config/index.js
+++ back/config/index.js
@@ -3,5 +3,6 @@ dotenv.config();
 
 module.exports = {
 	MELI_BASE_URL: process.env.MELI_BASE_URL,
-	PORT: process.env.PORT
+	PORT: process.env.PORT,
+	NODE_ENV: process.env.NODE_ENV
 };
--- back/server.js
+++ back/server.js
@@ -3,14 +3,17 @@ const path = require('path');
 
 const app = require('./setup-express');
 const routes = require('./routes');
-const { PORT } = require('./config');
+const { PORT, NODE_ENV } = require('./config');
 const setAuthor = require('./middlewares/setAuthor');
 
 app.use(cors());
 app.use('/api', [setAuthor, routes]);
-app.use('*', (req, res) => {
-	res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+
+if (NODE_ENV !== 'development') {
+	app.use('*', (req, res) => {
+		res.sendFile(path.join(__dirname, 'build', 'index.html'));
+	});
+}
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
 
