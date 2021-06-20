90f5f5296a59064e153320afbf6ca6263709fe46
Matias Manevi <manevimatias@gmail.com>
2021-06-20 19:04:22 -0300

    new build

--- back/server.js
+++ back/server.js
@@ -3,13 +3,13 @@ const path = require('path');
 
 const app = require('./setup-express');
 const routes = require('./routes');
-const { PORT, NODE_ENV } = require('./config');
+const { PORT = 5000, NODE_ENV } = require('./config');
 const setAuthor = require('./middlewares/setAuthor');
 
 app.use(cors());
 app.use('/api', [setAuthor, routes]);
 
-if (NODE_ENV === 'production') {
+if (NODE_ENV !== 'development') {
 	app.use('*', (req, res) => {
 		res.sendFile(path.join(__dirname, 'build', 'index.html'));
 	});
